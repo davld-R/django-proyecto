d089b7f5963e12d571b12b4e8f08eaade81bc4e1
Juan David Rojas <juanrojas043@gmail.com>
2023-04-26 13:58:34 -0500

    change url 2

--- proyecto5/urls.py
+++ proyecto5/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from re import template
+from django import views
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('azul.urls')),
