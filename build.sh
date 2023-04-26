def1ee6233197edd2eb19023a8bccbfd2e654629
Juan David Rojas <juanrojas043@gmail.com>
2023-04-26 12:28:39 -0500

    buildsh createuser

--- build.sh
+++ build.sh
@@ -7,3 +7,11 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+set -e
+
+echo "Running build script..."
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('david', 'david@gmail.com', 'david123')" | python manage.py shell
+
+echo "Build script finished."
