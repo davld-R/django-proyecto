20480fabd3659de8c7a3bf81ff25fab75371b94e
Juan David Rojas <juanrojas043@gmail.com>
2023-04-27 14:19:22 -0500

    bd

--- proyecto5/settings.py
+++ proyecto5/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'proyecto5.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-        'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/dbproyecto',conn_max_age=600)
+        'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/dbproyecto2',conn_max_age=600)
         
     #     'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
