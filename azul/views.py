f932cb2c6bdfea5b071f416e7f197d233ad623b7
Juan David Rojas <juanrojas043@gmail.com>
2023-04-27 10:38:26 -0500

    feed principal

--- azul/views.py
+++ azul/views.py
@@ -7,6 +7,9 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.db.models import Q
 
+def principal(request):
+    return render(request, 'principal.html')
+
 # TODO: Vista de Dumsterdiving
 @login_required
 def dumsterdiving(request):
